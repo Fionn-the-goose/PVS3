1163d22ec91c869e37d58a4c3a125b46356f2939
PeteTheN00b <ropeter63@googlemail.com>
2021-01-07 02:37:26 +0100

    GOLDEN WIND

--- matmult/matmultProject/matmultProject.cpp
+++ matmult/matmultProject/matmultProject.cpp
@@ -68,11 +68,6 @@ int main(int argc, char* argv[])
 	MPI_Comm_rank(MPI_COMM_WORLD, &nodeID);
 	MPI_Status status;
 
-	float** A, ** B, ** C;	// matrices
-	int d1, d2, d3;         // dimensions of matrices
-	int i, j, k;			// loop variables
-
-	//serial stuff
 	/* print user instruction */
 	if (argc != 4)
 	{
@@ -81,56 +76,172 @@ int main(int argc, char* argv[])
 		return 0;
 	}
 
+	int d1, d2, d3;         // dimensions of matrices
+	int i, j, k;			// loop variables
+
 	/* read user input */
 	d1 = atoi(argv[1]);		// rows of A and C
 	d2 = atoi(argv[2]);     // cols of A and rows of B
 	d3 = atoi(argv[3]);     // cols of B and C
 
-	printf("Matrix sizes C[%d][%d] = A[%d][%d] x B[%d][%d]\n", d1, d3, d1, d2, d2, d3);
+	float** A, ** B;
 
-	/* prepare matrices */
 	A = alloc_mat(d1, d2);
 	init_mat(A, d1, d2);
 	B = alloc_mat(d2, d3);
 	init_mat(B, d2, d3);
-	C = alloc_mat(d1, d3);	// no initialisation of C, because it gets filled by matmult
 
 	//parallel send / receive version
-	if (0 == nodeID)
-	{
-		printf("Receiving results from other tasks\n\n");
-		for (int i = 1; i < numNodes; ++i)
-		{
-			MPI_Recv(C[d1 * (i - 1) / (numNodes - 1)], d1 / (numNodes - 1), MPI_FLOAT, i, 1, MPI_COMM_WORLD, &status);
-		}
-	}
-	else
 	{
 		std::chrono::milliseconds start, end;
 
 		start = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
 
-		printf("Node %d: performing parallel matrix multiplication...\n", nodeID);
-		//for (i = d1 * nodeID / numNodes; i < (d1 * (nodeID + 1) / numNodes); i++)
-		for(i = 0; i < d1 / (numNodes - 1); ++i)
+		float ** C;	// matriCes
+
+		printf("Matrix sizes C[%d][%d] = A[%d][%d] x B[%d][%d]\n", d1, d3, d1, d2, d2, d3);
+
+		/* prepare matrices */
+		C = alloc_mat(d1, d3);	// no initialisation of C, because it gets filled by matmult
+
+		//float sendBuf[1];
+		//float recvBuf[1];
+
+		float* sendBuf = (float*)calloc(d2 > d3 ? d2 : d3, sizeof(float));
+		float* recvBuf = (float*)calloc(d2 > d3 ? d2 : d3, sizeof(float));
+
+		if (0 == nodeID)
 		{
-			//printf("%d ", i);
+			printf("Sending to other tasks\n\n");
+			{
+				std::chrono::milliseconds start, end;
 
-			for (j = 0; j < d3; j++)
-				for (k = 0; k < d2; k++)
-					C[i][j] += A[i][k] * B[k][j];
+				start = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+
+				for (int i = 1; i < numNodes; ++i)
+				{
+					for (int j = 0; j < d2; ++j)
+					{
+						sendBuf[j] = A[d1 * (i - 1) / (numNodes - 1)][j];
+					}
+					MPI_Send(sendBuf, d2, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
+				}
+				
+				end = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+
+				printf("\nParallel Send / Receive Time Taken To Send in Milliseconds: %lld\n\n\n", end.count() - start.count());
+			}
+
+			printf("Receiving results from other tasks\n\n");
+			for (int i = 1; i < numNodes; ++i)
+				for (int i2 = d1 * (nodeID - 1) / (numNodes - 1); i2 < (d1 * nodeID / (numNodes - 1)); i2++)
+				{
+					MPI_Recv(recvBuf, d3, MPI_FLOAT, i, 1, MPI_COMM_WORLD, &status);
+
+					for (int j = 0; j < d3; ++j)
+					{
+						C[d1 * (i - 1) / (numNodes - 1)][j] += recvBuf[j];
+					}
+				}
+
+			/*print_mat(A, d1, d2, "A");
+			print_mat(B, d2, d3, "B");
+			print_mat(C, d1, d3, "C");*/
 		}
+		else
+		{
+			//Calculation Part
+			{
+				std::chrono::milliseconds start, end;
 
-		MPI_Send(C[d1 * (nodeID - 1) / (numNodes - 1)], d1 / (numNodes - 1), MPI_FLOAT, 0, 1, MPI_COMM_WORLD);
+				MPI_Recv(recvBuf, d2, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &status);
+				for (int j = 0; j < d2; ++j)
+				{
+					A[0][j] = recvBuf[j];
+				}
+				//print_mat(A, d1, d2, "A");
+				start = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+
+				printf("Node %d: performing parallel matrix multiplication...\n", nodeID);
+				//for (i = d1 * (nodeID - 1) / (numNodes - 1); i < (d1 * nodeID / (numNodes - 1)); i++)
+				for (i = 0; i < d1 / (numNodes - 1); ++i)
+					for (j = 0; j < d3; j++)
+						for (k = 0; k < d2; k++)
+							C[i][j] += A[0][k] * B[k][j];
+
+				end = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+
+				printf("\nParallel Send / Receive Calculation Time Taken in Milliseconds: %lld\n\n\n", end.count() - start.count());
+			}
+
+			//Return Part
+			{
+				std::chrono::milliseconds start, end;
+
+				start = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+
+				for (i = 0; i < d1 / (numNodes - 1); ++i)
+				{
+					for (j = 0; j < d3; j++)
+					{
+						sendBuf[j] = C[i][j];
+					}
+
+					MPI_Send(sendBuf, d3, MPI_FLOAT, 0, 1, MPI_COMM_WORLD);
+				}
+
+				end = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+
+				printf("\nParallel Send / Receive Time Taken To Receive (Return Send) in Milliseconds: %lld\n\n\n", end.count() - start.count());
+			}
+		}
 
 		end = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
 
-		printf("\nParallel Send / Receive Time Taken in Milliseconds: %lld\n\n\n", end.count() - start.count());
+		printf("\nParallel Send / Receive Total Time Taken in Milliseconds: %lld\n\n\n", end.count() - start.count());
 	}
+
+	//parallel scatter version
+	//{
+	//	float** sendBuf = alloc_mat(d1 / (numNodes - 1), d3);
+	//	float* recvBuf = (float*)calloc(d1, sizeof(float*));
+
+	//	MPI_Scatter(sendBuf, d1, MPI_FLOAT, recvBuf, d1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+	//	
+	//	if(0 != nodeID)
+	//	{
+	//		std::chrono::milliseconds start, end;
+
+	//		start = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+
+	//		printf("Node %d: performing parallel matrix multiplication...\n", nodeID);
+	//		//for (i = d1 * nodeID / numNodes; i < (d1 * (nodeID + 1) / numNodes); i++)
+	//		for (i = 0; i < d1 / (numNodes - 1); ++i)
+	//		{
+	//			//printf("%d ", i);
+
+	//			for (j = 0; j < d3; j++)
+	//				for (k = 0; k < d2; k++)
+	//					C[i][j] += A[i][k] * B[k][j];
+	//		}
+
+	//		end = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+
+	//		printf("\nParallel Scatter Time Taken in Milliseconds: %lld\n\n\n", end.count() - start.count());
+	//	}
+	//	MPI_Gather(recvBuf, d1, MPI_FLOAT, sendBuf, d1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+	//}
 
 	/* serial version of matmult */
 	if (0 == nodeID)
 	{
+		float** C;	// matrices
+
+		printf("Matrix sizes C[%d][%d] = A[%d][%d] x B[%d][%d]\n", d1, d3, d1, d2, d2, d3);
+
+		/* prepare matrices */
+		C = alloc_mat(d1, d3);	// no initialisation of C, because it gets filled by matmult
+
 		std::chrono::milliseconds start, end;
 
 		start = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
@@ -143,12 +254,12 @@ int main(int argc, char* argv[])
 
 		end = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
 
-		printf("\nTime Taken in Milliseconds: %lld\n\n\n", end.count() - start.count());
+		printf("\nSerial Time Taken in Milliseconds: %lld\n\n\n", end.count() - start.count());
 
 		/* test output */
-		//print_mat(A, d1, d2, "A");
-		//print_mat(B, d2, d3, "B");
-		//print_mat(C, d1, d3, "C");
+		/*print_mat(A, d1, d2, "A");
+		print_mat(B, d2, d3, "B");
+		print_mat(C, d1, d3, "C");*/
 
 		printf("\nDone.\n");
 
